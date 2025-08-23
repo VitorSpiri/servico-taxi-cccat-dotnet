5e2b85bddfb6d3f83e376bac9fa1a89fd2c45b5c
Vitor Spiri <vitorspiri1@gmail.com>
2025-08-22 21:25:59 -0300

    feat: configuracao de projeto e solucao

---
+++ src/Servico-Taxi.Application/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Servico_Taxi_Online_Cccat_Dotnet.Application;
+
+public class Class1
+{
+}
---
+++ src/Servico-Taxi.Infrastructure/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Servico_Taxi_Online_Cccat_Dotnet.Infrastructure;
+
+public class Class1
+{
+}
