b6269bf1b411eb6eb929c42b8a4ac209f1e273c0
Vitor Spiri <vitorspiri1@gmail.com>
2025-08-24 11:33:30 -0300

    feat: método para validar cpf inválido

--- tests/UnitTests/ValidateCpfTests.cs
+++ tests/UnitTests/ValidateCpfTests.cs
@@ -4,19 +4,6 @@ namespace UnitTests;
 
 public class ValidateCpfTests
 {
-  [Fact]
-  public void ValidateCpf_CpfIsValid_ShouldReturnTrue()
-  {
-    //Arrange
-    var cpf = "52242477056";
-    
-    //Act
-    var validateCpf = ValidateCpf.IsValid(cpf);
-    
-    //Assert
-    Assert.True(validateCpf);
-  }
-
   [Theory]
   [InlineData("11144477735", true)]
   [InlineData("12345678901", false)]
@@ -24,10 +11,19 @@ public class ValidateCpfTests
   [InlineData("11111111111", false)]
   public void ValidateCpf_WithManyFormats_ShouldHaveExpectedResult(string cpf, bool expectedResult)
   {
-    // Act  
     var validateCpf = ValidateCpf.IsValid(cpf);
-    
-    //Assert
     Assert.Equal(expectedResult, validateCpf);
+  }
+  
+  [Theory]
+  [InlineData("")]
+  [InlineData(null)]
+  [InlineData("123")]
+  [InlineData("12345678901234")]
+  [InlineData("abcdefghijk")]
+  public void ValidateCpf_InvalidFormat_ShouldHaveReturnFlase(string invalidCpf)
+  {
+    var validateCpf = ValidateCpf.IsValid(invalidCpf);
+    Assert.False(validateCpf);
   }
 }
