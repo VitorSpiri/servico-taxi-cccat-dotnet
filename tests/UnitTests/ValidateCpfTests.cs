fbe11fa854dcf40c102e3c8a37d63d285c26169b
Vitor Spiri <vitorspiri1@gmail.com>
2025-08-24 12:09:38 -0300

    refactor: adicionando novos valores para teste de cpf

--- tests/UnitTests/ValidateCpfTests.cs
+++ tests/UnitTests/ValidateCpfTests.cs
@@ -5,7 +5,9 @@ namespace UnitTests;
 public class ValidateCpfTests
 {
   [Theory]
+  [InlineData("52242477056", true)]
   [InlineData("11144477735", true)]
+  [InlineData("11144477745", false)]
   [InlineData("12345678901", false)]
   [InlineData("00000000000", false)]
   [InlineData("11111111111", false)]
@@ -21,7 +23,7 @@ public class ValidateCpfTests
   [InlineData("123")]
   [InlineData("12345678901234")]
   [InlineData("abcdefghijk")]
-  public void ValidateCpf_InvalidFormat_ShouldHaveReturnFlase(string invalidCpf)
+  public void ValidateCpf_InvalidFormat_ShouldHaveReturnFalse(string invalidCpf)
   {
     var validateCpf = ValidateCpf.IsValid(invalidCpf);
     Assert.False(validateCpf);
